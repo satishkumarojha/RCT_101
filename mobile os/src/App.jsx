424ee93df6f2c4915ee59f736f0e7c547f4d7d20
satishkumarojha <satishkumarojha15@gmail.com>
2022-05-19 12:17:35 +0530

    mobile os created

---
+++ mobile os/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const os = ["Androied", "Blackberry", "iphone", "Windows phone"];
+  const brands = [
+    { name: "Samsung", type: "square" },
+    { name: "Htc", type: "square" },
+    { name: "Micromax", type: "disc" },
+    { name: "Apple", type: "circle" },
+  ];
+  const Heading = ({ props }) => {
+    return <h1>{props}</h1>;
+  };
+  const List = ({ list,type }) => {
+    return (
+      <ul>
+        <li className={type}>{list}</li>
+      </ul>
+    );
+  };
+  return (
+    <div className="App">
+      <Heading props={"Mobile operating sysytem"} />
+      {os.map((e) => {
+        return <List list={e} />;
+      })}
+      <Heading props={"Mobile Manufactures"} />
+      {brands.map((e)=>{
+        return  <List list={e.name} type={e.type}/>
+       
+      })}
+    </div>
+  );
+}
+
+export default App;
